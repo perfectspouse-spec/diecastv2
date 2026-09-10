19c98f19071fc7b03adce536d5a7f66c85c58bd6
Ersel Gücer <perfectspouse@gmail.com>
2026-09-11 01:09:22 +0300

    build: bump version to 29.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.aistudio.diecastcollection.m8x2k4"
     minSdk = 24
     targetSdk = 36
-    versionCode = 28
-    versionName = "28.0"
+    versionCode = 29
+    versionName = "29.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
